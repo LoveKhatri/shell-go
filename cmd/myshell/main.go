7a044c7368197cdf239067267bdd7ccddb461940
Love Khatri <lovekhatri9211@gmail.com>
2025-01-06 18:55:13 +0530

    Handle invalid commands#CZ2

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -14,5 +15,15 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Println("Error reading input: ", err)
+		os.Exit(1)
+	}
+
+	// Remove newline character from input
+	input = strings.TrimSpace(input)
+
+	// Print Invalid command
+	fmt.Printf("%s: command not found\n", input)
 }
